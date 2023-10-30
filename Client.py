bea9c8d98ebfaf22fb75d8939da3ebe941e838c3
skysky229 <huy.nguyen229@hcmut.edu.vn>
2023-10-30 22:23:45 +0700

    add fetch, discover and minor changes to client-server class

--- Client.py
+++ Client.py
@@ -1,29 +1,52 @@
-from ClientAPI.FetchFile import fetch
+from ClientAPI.FetchFile import fetch, fetch_from_client, discover
 from ClientAPI.PublishFile import publish
 from ClientAPI.ServerHandler import return_local_file
 from ClientAPI.SendFile import handle_fetch_request
 from UI.ClientUI import ClientUI
+import ast
+import socket
 
 class Client:
-  def __init__(self, IP_address, port, clientUI):
-    self.User_IP = IP_address
-    self.Port_num = port
+  def __init__(self, server_IP_address, server_port, client_IP_address, client_port, clientUI):
+    self.server_IP = server_IP_address
+    self.server_port = server_port
+    self.User_IP = client_IP_address
+    self.Port_num = client_port
     self.ClientUI = clientUI
+    self.published_file = []
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect((self.server_IP, self.server_port))
     print("init client")
   
   def handle_client(self):
     print("handle other client's request")
     
-  def handle_server(self):
-    return_local_file("servername")
-    
+  def handle_server(self, server_socket):
+    # Extract server address
+    server_address = server_socket.getpeername()
+    fetch(server_socket)
+
+    # Handle the messages from the server
+    while True: 
+        msg = server_socket.recv(1024).decode('utf-8')
+        if len(msg) == 0:
+              print("Connection closed by the server.")
+              break
+        print(f"Received message from {server_address}: {msg}")
+
+        # Check the type of message from client. 
+        msg = msg.split(None, 1)
+        if msg[0] == "discover":
+              discover(server_socket, self.published_file)     
   def handle_client_UI(self):
     publish("some file")
   
   def request_server_file(self):
     handle_fetch_request()
 
-clientUI = ClientUI()
-client1 = Client("192.168.1.8", 8000, clientUI)
-client1.handle_server()
-client1.ClientUI.display_available_file()
+if __name__ == "__main__": 
+  clientUI = ClientUI()
+  client1 = Client("127.0.0.1", 12345, "192.168.1.8", 8000, clientUI)
+  client1.handle_server()
+  client1.ClientUI.display_available_file()
--- ClientAPI/FetchFile.py
+++ ClientAPI/FetchFile.py
@@ -1,5 +1,44 @@
-def fetch(fname):
-  fetch_from_client(Client_socket, Client_IP, lname, fname)
+from ClientUI import display_available_file
+import ast
+import socket
 
-def fetch_from_client(Client_socket, Client_IP, lname, fname):
-  print("send fetch request to other client")
+def fetch(server_socket):
+     # Extract server address
+     server_address = server_socket.getpeername()
+
+     msg = "fetch"
+     server_socket.sendall(msg.encode('utf-8'))
+     print(f"Send fetch requests to server")
+
+     for _ in range(2):
+          msg = server_socket.recv(1024).decode('utf-8')
+          if len(msg) == 0:
+               print("Connection closed by the server.")
+               break
+          print(f"Received message from {server_address}: {msg}")
+
+          # Check the type of message from client. 
+          msg = msg.split(None, 1)
+          if msg[0] == "list": # return a list of available file from fetch request. User will choose a file, then client will send the request for info of that file
+               chosen_file = display_available_file(msg[1])
+               server_socket.sendall(f"{chosen_file}".encode('utf-8'))
+          elif msg[0] == "client": # return the info of a client having the chosen file
+               fetch_from_client(chosen_file, msg[1])
+
+
+def fetch_from_client(chosen_file, target_address):
+     print(target_address)
+     target_address = ast.literal_eval(target_address) # Convert to a tuple
+     fetch_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     fetch_socket.connect(target_address)
+
+     msg = f"fetch {chosen_file}"
+     fetch_socket.sendall(msg.encode('utf-8'))
+     with open(f'{chosen_file}.txt', 'wb') as file:
+          data = fetch_socket.recv(4096)  # Adjust buffer size as per your requirements
+          file.write(data)
+
+def discover(server_socket, published_file):
+     msg = ' '.join(map(str, published_file))
+     server_socket.sendall(msg.encode('utf-8'))
+     print(f"Send published filenames to server")
